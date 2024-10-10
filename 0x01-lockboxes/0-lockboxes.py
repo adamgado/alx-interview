f0d69419e6c61984b81acb7941d9a170da95bd34
adamgado <adamgado33@gmail.com>
2024-10-10 13:56:34 +0000

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,39 +1,16 @@
 #!/usr/bin/env python3
-"""
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1
-and each box may contain keys to the other boxes
-
-More info:
-- Prototype: def canUnlockAll(boxes)
-- boxes is a list of lists
-- A key with the same number as a box opens that box
-- You can assume all keys will be positive integers
-- The first box boxes[0] is unlocked
-- Return True if all boxes can be opened, else return False
-"""
+"""box numbered sequentially and each box may contain keys to other boxes"""
 
 
 def canUnlockAll(boxes):
-    """
-    Description:
-    Write a method that determines if all the boxes can be opened
-    Arguments:
-    boxes --> List of Lists, it contains the boxes with keys
-    Reurn boolean
-    Variables:
-    myKeys --> List, Store the number keys to open boxes
-    key --> integer, key of the myKeys
-    boxKey --> iinteger, key inside of an specific box
-	"""
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    """determine if all the boxes can be opened"""
+    keyslist = [0]
+    for a in keyslist:
+        for b in boxes[a]:
+            if b not in keyslist and b < len(boxes):
+                keyslist.append(b)
+
+    if len(keyslist) == len(boxes):
+        return True
+    else:
+        return False
